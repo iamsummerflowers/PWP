31f9480a5bd5b3561a30c853192f156cb5c253c8
Summer <summer.flowers94@gmail.com>
2025-10-24 17:01:10 -0500

    Gallery Updates + Tik Tok Link
    Gallery Updates + Tik Tok Link

--- startdev/src/Data/gallerydata.js
+++ startdev/src/Data/gallerydata.js
@@ -1,7 +1,7 @@
-import image1 from "../assets/brittany.jpeg";
+import image1 from "../assets/fivepose.jpeg";
 import image2 from "../assets/grapevine.jpeg";
 import image3 from "../assets/kayla.png";
-import image4 from "../assets/fivepose.jpeg";
+import image4 from "../assets/ninepose.png";
 import image5 from "../assets/threeawards.png";
 import image6 from "../assets/ninepose.png";
 import image7 from "../assets/juneteenth.png";
@@ -15,22 +15,22 @@ import image11 from "../assets/communitynight.jpeg";
 
 export const galleryImages = [
         {
-            src: image4,
-            alt: "Young woman speaking on a podium",
-            title: "Flag Raising 2025",
-            // description: "Brittany is a driven and creative 17-year-old with a strong passion for beauty, fashion and entrepreneurship. Since 2020, she has been exercising her skill to create hair and nail designs, known for her unique concepts and artistic flair. Brittany enjoys creating standout looks and bringing original ideas to life. "
+            src: image1,
+            alt: "Milpitas Juneteenth 2024",
+            title: "Milpitas Juneteenth",
+            description: "2024"
         },
         {
             src: image2,
-            alt: "Grapevine Award",
-            title: "Flag Raising 2025t",
-            // description: "Another short text blurb with additional information"
+            alt: "Grant from Grapevine",
+            title: "Grant from Grapevine",
+            description: "2025"
         },
         {
             src: image3,
-            alt: "Junteenth flyer for 2024",
-            title: "Flag Raising 2025",
-            // description: "Teens developing leadership skills"
+            alt: "Kayla Choates, Mistress of Ceremonies for Milpitas Juneteenth 2025",
+            title: "Kayla Choates, Mistress of Ceremonies for Milpitas Juneteenth",
+            description: "2025"
         },
         // {
         //     src: image1,
@@ -40,44 +40,44 @@ export const galleryImages = [
         // },
         {
             src: image5,
-            alt: "Booth owner of Rose Mary's Gourmet Pies working at a tent",
-            title: "Flag Raising 2025",
+            alt: "REMOVE",
+            title: "REMOVE",
             // description: "Building stronger family bonds"
         },
         {
-            src: image6,
-            alt: "Community members taking a picture for an award",
-            title: "Flag Raising 2025",
-            // description: "Interactive parenting education session"
+            src: image4,
+            alt: "Milpitas Flag Raising Ceremony",
+            title: "Milpitas Flag Raising Ceremony",
+            description: "February 2024"
         },
         {
             src: image7,
-            alt: "Commnity member speaking on a stage at a neighbor hood event",
-            title: "Flag Raising 2025",
-            // description: "Annual family celebration gathering"
+            alt: "Milpitas Flag Raising Ceremony",
+            title: "Milpitas Flag Raising Ceremony",
+            description: "February 2025"
         },
         {
             src: image8,
-            alt: "Young woman speaking on a podium",
-            title: "Flag Raising 2025",
-            // description: "Experienced parent mentoring newcomer"
+            alt: "Milpitas Juneteenth 2024",
+            title: "Milpitas Juneteenth",
+            description: "2024"
         },
         {
             src: image9,
-            alt: "Flyer with speakers showcased",
-            title: "Flag Raising 2025",
-            // description: "Experienced parent mentoring newcomer"
+            alt: "Our Founder on a Back Black Speaking Panel",
+            title: "Our Founder on a Back Black Speaking Panel",
+            // description: "2024"
         },
             {
             src: image10,
-            alt: "Woman at a booth",
-            title: "Flag Raising 2025",
+            alt: "Our Founder on a Back Black Speaking Panel",
+            title: "Our Founder on a Back Black Speaking Panel",
             // description: "Experienced parent mentoring newcomer"
         },
                 {
             src: image11,
-            alt: "Flyer showcasing a community event",
-            title: "Flag Raising 2025",
+            alt: "Community Night wit MUSD",
+            title: "Community Night wit MUSD",
             // description: "Experienced parent mentoring newcomer"
         }
     ];
